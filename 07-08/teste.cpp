bb56867486a6a7ee73e32aef485f159e2eb1a643
Arthur <arthurschiller10@gmail.com>
2025-08-07 09:01:05 -0300

    first commit

---
+++ 07-08/teste.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // lib for input/output
+#include <cstdio> // lib for C-style input/output
+
+
+using namespace std; 
+
+int main() {
+    // 
+    printf("Hello, World!\n");
+    return 0;
+}
