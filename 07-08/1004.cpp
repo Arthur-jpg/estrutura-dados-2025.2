72b6a50ef7bea76188f29e29a7d454c423e58182
Arthur Schiller <arthurschiller@MacBook-Pro-de-Arthur.local>
2025-08-09 16:22:17 -0300

    1004

---
+++ 07-08/1004.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int produto = a * b;
+    printf("PROD = %d\n", produto);
+
+
+    return 0;
+}
