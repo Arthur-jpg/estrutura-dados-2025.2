80feed04543004e8b4cb73b95442fe400d0f4262
Arthur Schiller <arthurschiller10@gmail.com>
2025-08-14 11:12:42 -0300

    final aula

---
+++ 14-08/fibo_iterativa.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+long long int F [10000];
+
+void fibo(int N) {
+    for(int i = 2; i <= N; i++){
+        F[i] = F[i-1] + F[i-2];
+    }
+}
+
+
+void inicializa_vertor() {
+    F[0] = -1;
+    F[1] = -1;
+}
+
+int main() {
+    inicializa_vertor();
+    fibo(10000);
+
+    int x;
+    cin >> x;
+    cout << F[x]<< endl;
+    return 0;
+}
